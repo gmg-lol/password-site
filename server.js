4eac0023f6ebf875545e88f88df5f2f7aad4c3b1
gmg-lol <growmygarden1972@gmail.com>
2026-03-25 20:42:55 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// DB 연결
+mongoose.connect("mongodb://127.0.0.1:27017/secureDB");
+
+// 데이터 모델
+const Data = mongoose.model("Data", {
+  content: String
+});
+
+// 저장
+app.post("/save", async (req, res) => {
+  await Data.deleteMany();
+  await Data.create({ content: req.body.content });
+  res.send("저장 완료");
+});
+
+// 불러오기
+app.get("/get", async (req, res) => {
+  const data = await Data.findOne();
+  res.json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("서버 실행됨"));
